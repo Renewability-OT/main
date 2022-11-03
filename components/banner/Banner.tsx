677ee80bfd8f9b65e2e2894bbd5925ba509b4002
Colin Breeding <93558819+colinmb5@users.noreply.github.com>
2022-11-03 13:59:53 -0400

    update banner

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -3,31 +3,34 @@ import React from "react";
 
 export const Banner = () => {
   return (
-    <div className="w-full flex justify-center">
-      <div className="mt-24 w-[1180px] h-[680px] rounded-3xl rounded-br-[300px] bg-gradient-to-r from-green to-lightBlue">
-        <p className="absolute uppercase text-white font-extrabold text-[56px] text-center w-[28rem] top-48 left-52 drop-shadow-3xl leading-[4.5rem]">
-          Occupational Therapy <br />
-          For All Ages
-        </p>
-        <p className="absolute top-[26rem] text-[22px] left-[17.3rem] drop-shadow-4xl">
-          Believing In The Power Of You!
-        </p>
-        <Link
-          href=""
-          className="absolute top-[29rem] text-white font-bold left-[17.8rem] border py-2 px-7 rounded-xl transition ease-in-out duration-300 shadow-Button hover:shadow-darkButton"
-        >
-          Book Now
-        </Link>
-        <Link
-          href=""
-          className="absolute top-[29rem] text-white font-bold left-[26.8rem] border py-2 px-5 rounded-xl transition ease-in-out duration-300 shadow-Button hover:shadow-darkButton"
-        >
-          Our Services
-        </Link>
-        <img
-          src="/assets/illustrations/Lifesavers-Caretaking.png"
-          className="w-108 absolute bottom-[-6rem] right-36"
-        />
+    <div className="w-full flex justify-center px-12">
+      <div className="grid grid-row-2 lg:grid-cols-2 items-center mt-24 w-[1180px] h-[680px] rounded-3xl rounded-br-[300px] bg-gradient-to-r from-green to-lightBlue shadow-banner">
+        <div className="text-center mb-36 ml-20">
+          <p className="uppercase text-white font-extrabold text-[56px] drop-shadow-3xl leading-[4.5rem]">
+            Occupational Therapy <br />
+            For All Ages
+          </p>
+          <p className="text-[22px] drop-shadow-4xl mt-4">
+            Believing In The Power Of You!
+          </p>
+          <div className="mt-6 ">
+            <Link
+              href=""
+              className="text-white font-bold border py-3 px-7 rounded-xl transition ease-in-out duration-300 shadow-Button hover:shadow-darkButton mr-2"
+            >
+              Book Now
+            </Link>
+            <Link
+              href=""
+              className="text-white font-bold border py-3 px-5 rounded-xl transition ease-in-out duration-300 shadow-Button hover:shadow-darkButton"
+            >
+              Our Services
+            </Link>
+          </div>
+        </div>
+        <div className="mt-56">
+          <img src="/assets/illustrations/Lifesavers-Caretaking.png" />
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
       boxShadow: {
         Button: "0px 4px 4px rgba(0, 0, 0, 0.25)",
         darkButton: "0px 4px 4px rgba(0, 0, 0, 0.50)",
+        banner: "10px 10px 22px -7px rgba(0,0,0,0.62)",
       },
       dropShadow: {
         "3xl": "0px 4px 4px rgba(0, 0, 0, 0.25)",
